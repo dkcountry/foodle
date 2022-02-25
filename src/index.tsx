c4428be774f25bc39402b991bcb2a6695b528522
David Kang <david@keepertax.com>
2022-02-24 16:59:30 -0800

    re-add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import HttpsRedirect from 'react-https-redirect'
 import './index.css'
 import App from './App'
+import amplitude from 'amplitude-js'
+
+amplitude.getInstance().init(process.env.REACT_APP_ANALYTICS_KEY || '')
 
 ReactDOM.render(
   <React.StrictMode>
