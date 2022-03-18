38a0f17aa377c7a1e70b99c1dc989a221614db37
Xinyun Rong <xinyunrong@Xinyuns-MacBook-Air.local>
2022-03-18 00:53:38 -0400

    add google search api

---
+++ pie/google_image.py
@@ -0,0 +1,25 @@
+from pie.word_list import food_list
+from google_images_search import GoogleImagesSearch
+
+gis = GoogleImagesSearch('AIzaSyCo4jJU96MvuUm9wN7y_5KzyZZLMdffm4Q', '7a387b681168bfee0')
+
+for food in food_list[0:5]:
+    path_to_dir = '/Users/xinyunrong/Desktop/Code/foodle/pie/images/{}'.format(food)
+
+    _search_params = {
+        'q': food,
+        'num': 20,
+        'fileType': 'png',
+        'imgSize': 'large',
+        'imgType': 'photo',
+        'aspectRatio': 'square'
+    }
+
+    gis.search(search_params=_search_params)
+
+    images = gis.results()
+    for img in images:
+        img.download(path_to_dir)
+        img = img.resize(320, 320)
+
+
