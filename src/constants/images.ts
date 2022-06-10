f455ac4eb425c50bc3ea4e8148b52cbe7563e690
David Kang <david@keepertax.com>
2022-06-09 21:55:56 -0500

    add image map

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,8 +1,575 @@
-import Orange from '../assets/orange.png'
+import Chips from '../assets/chips.png'
+import Tater from '../assets/tater.png'
+import Sugar from '../assets/sugar.png'
+import Gumbo from '../assets/gumbo.png'
+import Crepe from '../assets/crepe.png'
+import Curds from '../assets/curds.png'
+import Rolls from '../assets/rolls.png'
+import Slurp from '../assets/slurp.png'
+import Mochi from '../assets/mochi.png'
+import Ramen from '../assets/ramen.png'
+import Drink from '../assets/drink.png'
+import Basil from '../assets/basil.png'
+import Tofus from '../assets/tofus.png'
+import Wings from '../assets/wings.png'
+import Queso from '../assets/queso.png'
+import Vodka from '../assets/vodka.png'
+import Chili from '../assets/chili.png'
+import Tangy from '../assets/tangy.png'
+import Tasty from '../assets/tasty.png'
+import Jamon from '../assets/jamon.png'
+import Dhaba from '../assets/dhaba.png'
+import Puree from '../assets/puree.png'
+import Brine from '../assets/brine.png'
+import Prune from '../assets/prune.png'
+import Paste from '../assets/paste.png'
+import Nacho from '../assets/nacho.png'
+import Squid from '../assets/squid.png'
+import Pinot from '../assets/pinot.png'
+import Mince from '../assets/mince.png'
+import Choco from '../assets/choco.png'
+import Lambs from '../assets/lambs.png'
+import Lards from '../assets/lards.png'
+import Ripen from '../assets/ripen.png'
+import Dough from '../assets/dough.png'
+import Halal from '../assets/halal.png'
+import Cajun from '../assets/cajun.png'
+import Agave from '../assets/agave.png'
+import Snack from '../assets/snack.png'
+import Colas from '../assets/colas.png'
+import Saute from '../assets/saute.png'
+import Mirin from '../assets/mirin.png'
+import Wafer from '../assets/wafer.png'
+import Peach from '../assets/peach.png'
+import Tripe from '../assets/tripe.png'
+import Mints from '../assets/mints.png'
+import Bites from '../assets/bites.png'
+import Sweet from '../assets/sweet.png'
+import Clove from '../assets/clove.png'
+import Creme from '../assets/creme.png'
+import Roast from '../assets/roast.png'
+import Bacon from '../assets/bacon.png'
+import Bries from '../assets/bries.png'
+import Wines from '../assets/wines.png'
+import Caper from '../assets/caper.png'
+import Milky from '../assets/milky.png'
+import Trout from '../assets/trout.png'
+import Brews from '../assets/brews.png'
+import Snail from '../assets/snail.png'
+import Konbu from '../assets/konbu.png'
+import Chefs from '../assets/chefs.png'
+import Thyme from '../assets/thyme.png'
+import Pupus from '../assets/pupus.png'
+import Filet from '../assets/filet.png'
+import Taros from '../assets/taros.png'
+import Chops from '../assets/chops.png'
+import Mocha from '../assets/mocha.png'
+import Penne from '../assets/penne.png'
+import Leeks from '../assets/leeks.png'
+import Mango from '../assets/mango.png'
+import Bagel from '../assets/bagel.png'
+import Melon from '../assets/melon.png'
+import Ovens from '../assets/ovens.png'
+import Zesty from '../assets/zesty.png'
+import Pinto from '../assets/pinto.png'
+import Stove from '../assets/stove.png'
+import Gouda from '../assets/gouda.png'
+import Ladle from '../assets/ladle.png'
+import Llama from '../assets/llama.png'
+import Guava from '../assets/guava.png'
+import Farro from '../assets/farro.png'
+import Cakes from '../assets/cakes.png'
+import Dulce from '../assets/dulce.png'
+import Juice from '../assets/juice.png'
+import Apple from '../assets/apple.png'
+import Kombu from '../assets/kombu.png'
+import Sumac from '../assets/sumac.png'
+import Treat from '../assets/treat.png'
+import Aspic from '../assets/aspic.png'
+import Clams from '../assets/clams.png'
+import Ducks from '../assets/ducks.png'
+import Kelps from '../assets/kelps.png'
+import Pizza from '../assets/pizza.png'
+import Order from '../assets/order.png'
+import Yolky from '../assets/yolky.png'
+import Bison from '../assets/bison.png'
+import Shell from '../assets/shell.png'
+import Latte from '../assets/latte.png'
+import Toast from '../assets/toast.png'
+import Berry from '../assets/berry.png'
+import Lager from '../assets/lager.png'
+import Poori from '../assets/poori.png'
+import Jello from '../assets/jello.png'
+import Wraps from '../assets/wraps.png'
+import Maple from '../assets/maple.png'
+import Broil from '../assets/broil.png'
+import Gummy from '../assets/gummy.png'
+import Wagyu from '../assets/wagyu.png'
+import Tbone from '../assets/tbone.png'
+import Roots from '../assets/roots.png'
+import Plums from '../assets/plums.png'
+import Acorn from '../assets/acorn.png'
+import Beets from '../assets/beets.png'
+import Prawn from '../assets/prawn.png'
+import Rojak from '../assets/rojak.png'
+import Crumb from '../assets/crumb.png'
+import Cider from '../assets/cider.png'
+import Chard from '../assets/chard.png'
+import Slaws from '../assets/slaws.png'
+import Riced from '../assets/riced.png'
+import Ranch from '../assets/ranch.png'
+import Liver from '../assets/liver.png'
+import Rinds from '../assets/rinds.png'
 import Peels from '../assets/peels.png'
+import Sours from '../assets/sours.png'
+import Broth from '../assets/broth.png'
+import Whisk from '../assets/whisk.png'
+import Swill from '../assets/swill.png'
+import Nandu from '../assets/nandu.png'
+import Quart from '../assets/quart.png'
+import Crust from '../assets/crust.png'
+import Acais from '../assets/acais.png'
+import Cocoa from '../assets/cocoa.png'
+import Kebab from '../assets/kebab.png'
+import Grape from '../assets/grape.png'
+import Beans from '../assets/beans.png'
+import Fried from '../assets/fried.png'
+import Spicy from '../assets/spicy.png'
+import Feast from '../assets/feast.png'
+import Curry from '../assets/curry.png'
+import Syrup from '../assets/syrup.png'
+import Pesto from '../assets/pesto.png'
+import Whelk from '../assets/whelk.png'
+import Bread from '../assets/bread.png'
+import Jerky from '../assets/jerky.png'
+import Jelly from '../assets/jelly.png'
+import Bowls from '../assets/bowls.png'
+import Pasty from '../assets/pasty.png'
+import Mushy from '../assets/mushy.png'
+import Shake from '../assets/shake.png'
+import Chunk from '../assets/chunk.png'
+import Lunch from '../assets/lunch.png'
+import Kiwis from '../assets/kiwis.png'
+import Pints from '../assets/pints.png'
+import Baked from '../assets/baked.png'
+import Fatty from '../assets/fatty.png'
+import Glaze from '../assets/glaze.png'
+import Grind from '../assets/grind.png'
+import Hazel from '../assets/hazel.png'
+import Ponzu from '../assets/ponzu.png'
+import Malts from '../assets/malts.png'
+import Sushi from '../assets/sushi.png'
+import Poach from '../assets/poach.png'
+import Cakey from '../assets/cakey.png'
+import Pears from '../assets/pears.png'
+import Maize from '../assets/maize.png'
+import Dates from '../assets/dates.png'
+import Crabs from '../assets/crabs.png'
+import Chive from '../assets/chive.png'
+import Zuppa from '../assets/zuppa.png'
+import Aioli from '../assets/aioli.png'
+import Honey from '../assets/honey.png'
+import Meals from '../assets/meals.png'
+import Meats from '../assets/meats.png'
+import Spork from '../assets/spork.png'
+import Fudge from '../assets/fudge.png'
+import Gyoza from '../assets/gyoza.png'
+import Slice from '../assets/slice.png'
+import Cafes from '../assets/cafes.png'
+import Unagi from '../assets/unagi.png'
+import Quail from '../assets/quail.png'
+import Dills from '../assets/dills.png'
+import Smore from '../assets/smore.png'
+import Fungi from '../assets/fungi.png'
+import Scoop from '../assets/scoop.png'
+import Cacao from '../assets/cacao.png'
+import Pepsi from '../assets/pepsi.png'
+import Patty from '../assets/patty.png'
+import Flour from '../assets/flour.png'
+import Yuzus from '../assets/yuzus.png'
+import Lemon from '../assets/lemon.png'
+import Poboy from '../assets/poboy.png'
+import Cream from '../assets/cream.png'
+import Yeast from '../assets/yeast.png'
+import Knife from '../assets/knife.png'
+import Eater from '../assets/eater.png'
+import Savor from '../assets/savor.png'
+import Candy from '../assets/candy.png'
+import Thigh from '../assets/thigh.png'
+import Stews from '../assets/stews.png'
+import Crisp from '../assets/crisp.png'
+import Salsa from '../assets/salsa.png'
+import Baste from '../assets/baste.png'
+import Veals from '../assets/veals.png'
+import Kugel from '../assets/kugel.png'
+import Grill from '../assets/grill.png'
+import Salad from '../assets/salad.png'
+import Tikka from '../assets/tikka.png'
+import Mezze from '../assets/mezze.png'
+import Matzo from '../assets/matzo.png'
+import Salty from '../assets/salty.png'
+import Gravy from '../assets/gravy.png'
+import Anise from '../assets/anise.png'
+import Boars from '../assets/boars.png'
+import Pitas from '../assets/pitas.png'
+import Bento from '../assets/bento.png'
+import Fries from '../assets/fries.png'
+import Water from '../assets/water.png'
+import Udons from '../assets/udons.png'
+import Pecan from '../assets/pecan.png'
+import Tarts from '../assets/tarts.png'
+import Beers from '../assets/beers.png'
+import Saucy from '../assets/saucy.png'
+import Donut from '../assets/donut.png'
+import Natto from '../assets/natto.png'
+import Chewy from '../assets/chewy.png'
+import Torta from '../assets/torta.png'
+import Tapas from '../assets/tapas.png'
+import Lassi from '../assets/lassi.png'
+import Salts from '../assets/salts.png'
+import Egged from '../assets/egged.png'
+import Onion from '../assets/onion.png'
+import Seeds from '../assets/seeds.png'
+import Yummy from '../assets/yummy.png'
+import Wurst from '../assets/wurst.png'
+import Icing from '../assets/icing.png'
+import Poppy from '../assets/poppy.png'
+import Cumin from '../assets/cumin.png'
+import Plant from '../assets/plant.png'
+import Belly from '../assets/belly.png'
+import Enoki from '../assets/enoki.png'
+import Juicy from '../assets/juicy.png'
+import Spoon from '../assets/spoon.png'
+import Minty from '../assets/minty.png'
+import Pasta from '../assets/pasta.png'
+import Brats from '../assets/brats.png'
+import Beefy from '../assets/beefy.png'
+import Gyros from '../assets/gyros.png'
+import Vegan from '../assets/vegan.png'
+import Grits from '../assets/grits.png'
+import Spice from '../assets/spice.png'
+import Kvass from '../assets/kvass.png'
+import Herbs from '../assets/herbs.png'
+import Wheat from '../assets/wheat.png'
+import Carbs from '../assets/carbs.png'
+import Fetta from '../assets/fetta.png'
+import Serve from '../assets/serve.png'
+import Tacos from '../assets/tacos.png'
+import Snake from '../assets/snake.png'
+import Meaty from '../assets/meaty.png'
+import Grain from '../assets/grain.png'
+import Sides from '../assets/sides.png'
+import Fruit from '../assets/fruit.png'
+import Diner from '../assets/diner.png'
+import Refry from '../assets/refry.png'
+import Boozy from '../assets/boozy.png'
+import Shoyu from '../assets/shoyu.png'
+import Shank from '../assets/shank.png'
+import Sobas from '../assets/sobas.png'
+import Plate from '../assets/plate.png'
+import Fishy from '../assets/fishy.png'
+import Crops from '../assets/crops.png'
+import Tongs from '../assets/tongs.png'
+import Nutty from '../assets/nutty.png'
+import Fresh from '../assets/fresh.png'
+import Aroma from '../assets/aroma.png'
+import Scone from '../assets/scone.png'
+import Satay from '../assets/satay.png'
+import Fizzy from '../assets/fizzy.png'
+import Olive from '../assets/olive.png'
+import Soupy from '../assets/soupy.png'
+import Steak from '../assets/steak.png'
+import Salak from '../assets/salak.png'
+import Decaf from '../assets/decaf.png'
+import Steam from '../assets/steam.png'
+import Sauce from '../assets/sauce.png'
+import Porks from '../assets/porks.png'
 
 export const imageMap = {
-  orange: Orange,
+  chips: Chips,
+  tater: Tater,
+  sugar: Sugar,
+  gumbo: Gumbo,
+  crepe: Crepe,
+  curds: Curds,
+  rolls: Rolls,
+  slurp: Slurp,
+  mochi: Mochi,
+  ramen: Ramen,
+  drink: Drink,
+  basil: Basil,
+  tofus: Tofus,
+  wings: Wings,
+  queso: Queso,
+  vodka: Vodka,
+  chili: Chili,
+  tangy: Tangy,
+  tasty: Tasty,
+  jamon: Jamon,
+  dhaba: Dhaba,
+  puree: Puree,
+  brine: Brine,
+  prune: Prune,
+  paste: Paste,
+  nacho: Nacho,
+  squid: Squid,
+  pinot: Pinot,
+  mince: Mince,
+  choco: Choco,
+  lambs: Lambs,
+  lards: Lards,
+  ripen: Ripen,
+  dough: Dough,
+  halal: Halal,
+  cajun: Cajun,
+  agave: Agave,
+  snack: Snack,
+  colas: Colas,
+  saute: Saute,
+  mirin: Mirin,
+  wafer: Wafer,
+  peach: Peach,
+  tripe: Tripe,
+  mints: Mints,
+  bites: Bites,
+  sweet: Sweet,
+  clove: Clove,
+  creme: Creme,
+  roast: Roast,
+  bacon: Bacon,
+  bries: Bries,
+  wines: Wines,
+  caper: Caper,
+  milky: Milky,
+  trout: Trout,
+  brews: Brews,
+  snail: Snail,
+  konbu: Konbu,
+  chefs: Chefs,
+  thyme: Thyme,
+  pupus: Pupus,
+  filet: Filet,
+  taros: Taros,
+  chops: Chops,
+  mocha: Mocha,
+  penne: Penne,
+  leeks: Leeks,
+  mango: Mango,
+  bagel: Bagel,
+  melon: Melon,
+  ovens: Ovens,
+  zesty: Zesty,
+  pinto: Pinto,
+  stove: Stove,
+  gouda: Gouda,
+  ladle: Ladle,
+  llama: Llama,
+  guava: Guava,
+  farro: Farro,
+  cakes: Cakes,
+  dulce: Dulce,
+  juice: Juice,
+  apple: Apple,
+  kombu: Kombu,
+  sumac: Sumac,
+  treat: Treat,
+  aspic: Aspic,
+  clams: Clams,
+  ducks: Ducks,
+  kelps: Kelps,
+  pizza: Pizza,
+  order: Order,
+  yolky: Yolky,
+  bison: Bison,
+  shell: Shell,
+  latte: Latte,
+  toast: Toast,
+  berry: Berry,
+  lager: Lager,
+  poori: Poori,
+  jello: Jello,
+  wraps: Wraps,
+  maple: Maple,
+  broil: Broil,
+  gummy: Gummy,
+  wagyu: Wagyu,
+  tbone: Tbone,
+  roots: Roots,
+  plums: Plums,
+  acorn: Acorn,
+  beets: Beets,
+  prawn: Prawn,
+  rojak: Rojak,
+  crumb: Crumb,
+  cider: Cider,
+  chard: Chard,
+  slaws: Slaws,
+  riced: Riced,
+  ranch: Ranch,
+  liver: Liver,
+  rinds: Rinds,
   peels: Peels,
-  milks: Orange,
+  sours: Sours,
+  broth: Broth,
+  whisk: Whisk,
+  swill: Swill,
+  nandu: Nandu,
+  quart: Quart,
+  crust: Crust,
+  acais: Acais,
+  cocoa: Cocoa,
+  kebab: Kebab,
+  grape: Grape,
+  beans: Beans,
+  fried: Fried,
+  spicy: Spicy,
+  feast: Feast,
+  curry: Curry,
+  syrup: Syrup,
+  pesto: Pesto,
+  whelk: Whelk,
+  bread: Bread,
+  jerky: Jerky,
+  jelly: Jelly,
+  bowls: Bowls,
+  pasty: Pasty,
+  mushy: Mushy,
+  shake: Shake,
+  chunk: Chunk,
+  lunch: Lunch,
+  kiwis: Kiwis,
+  pints: Pints,
+  baked: Baked,
+  fatty: Fatty,
+  glaze: Glaze,
+  grind: Grind,
+  hazel: Hazel,
+  ponzu: Ponzu,
+  malts: Malts,
+  sushi: Sushi,
+  poach: Poach,
+  cakey: Cakey,
+  pears: Pears,
+  maize: Maize,
+  dates: Dates,
+  crabs: Crabs,
+  chive: Chive,
+  zuppa: Zuppa,
+  aioli: Aioli,
+  honey: Honey,
+  meals: Meals,
+  meats: Meats,
+  spork: Spork,
+  fudge: Fudge,
+  gyoza: Gyoza,
+  slice: Slice,
+  cafes: Cafes,
+  unagi: Unagi,
+  quail: Quail,
+  dills: Dills,
+  smore: Smore,
+  fungi: Fungi,
+  scoop: Scoop,
+  cacao: Cacao,
+  pepsi: Pepsi,
+  patty: Patty,
+  flour: Flour,
+  yuzus: Yuzus,
+  lemon: Lemon,
+  poboy: Poboy,
+  cream: Cream,
+  yeast: Yeast,
+  knife: Knife,
+  eater: Eater,
+  savor: Savor,
+  candy: Candy,
+  thigh: Thigh,
+  stews: Stews,
+  crisp: Crisp,
+  salsa: Salsa,
+  baste: Baste,
+  veals: Veals,
+  kugel: Kugel,
+  grill: Grill,
+  salad: Salad,
+  tikka: Tikka,
+  mezze: Mezze,
+  matzo: Matzo,
+  salty: Salty,
+  gravy: Gravy,
+  anise: Anise,
+  boars: Boars,
+  pitas: Pitas,
+  bento: Bento,
+  fries: Fries,
+  water: Water,
+  udons: Udons,
+  pecan: Pecan,
+  tarts: Tarts,
+  beers: Beers,
+  saucy: Saucy,
+  donut: Donut,
+  natto: Natto,
+  chewy: Chewy,
+  torta: Torta,
+  tapas: Tapas,
+  lassi: Lassi,
+  salts: Salts,
+  egged: Egged,
+  onion: Onion,
+  seeds: Seeds,
+  yummy: Yummy,
+  wurst: Wurst,
+  icing: Icing,
+  poppy: Poppy,
+  cumin: Cumin,
+  plant: Plant,
+  belly: Belly,
+  enoki: Enoki,
+  plate: Plate,
+  juicy: Juicy,
+  spoon: Spoon,
+  minty: Minty,
+  pasta: Pasta,
+  brats: Brats,
+  beefy: Beefy,
+  gyros: Gyros,
+  vegan: Vegan,
+  grits: Grits,
+  spice: Spice,
+  kvass: Kvass,
+  herbs: Herbs,
+  wheat: Wheat,
+  carbs: Carbs,
+  fetta: Fetta,
+  serve: Serve,
+  tacos: Tacos,
+  snake: Snake,
+  meaty: Meaty,
+  grain: Grain,
+  sides: Sides,
+  fruit: Fruit,
+  diner: Diner,
+  refry: Refry,
+  boozy: Boozy,
+  shoyu: Shoyu,
+  shank: Shank,
+  sobas: Sobas,
+  fishy: Fishy,
+  crops: Crops,
+  tongs: Tongs,
+  nutty: Nutty,
+  fresh: Fresh,
+  aroma: Aroma,
+  scone: Scone,
+  satay: Satay,
+  fizzy: Fizzy,
+  olive: Olive,
+  soupy: Soupy,
+  steak: Steak,
+  salak: Salak,
+  decaf: Decaf,
+  steam: Steam,
+  sauce: Sauce,
+  porks: Porks,
 }
